48401fb39d3d6e879681dfc280b4909748912b67
shapovalov-k <shapovalovkv@gmail.com>
2019-01-12 16:58:01 +0100

    first alpha version ready

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.4'
 gem 'rails', '~> 5.2.1', '>= 5.2.1.1'
+gem 'devise'
+
 
 gem 'twitter-bootstrap-rails', '~> 3.2', '>= 3.2.2'
 
@@ -10,8 +12,6 @@ gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
-
-gem 'jbuilder', '~> 2.5'
 
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,32 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  # Встроенный в девайз фильтр — посылает незалогиненного пользователя
+  before_action :authenticate_user!, except: [:show]
 
-  # GET /users/1
-  # GET /users/1.json
+  # Задаем объект @user для шаблонов и экшенов
+  before_action :set_current_user, except: [:show]
+
   def show
+    @user = User.find(params[:id])
   end
 
-  # GET /users/1/edit
   def edit
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to @user, notice: I18n.t('controllers.users.updated')
+    else
+      render :edit
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
+  def set_current_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
