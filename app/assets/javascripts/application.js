205455617ac4ac7ddf30b6abdd3e4b7d73f0dcc5
shapovalov-k <shapovalovkv@gmail.com>
2019-01-17 20:01:43 +0100

    comments feature added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require twitter/bootstrap
+//= require twitter/bootstrap/alert
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
